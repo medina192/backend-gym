7e0143d9fdb02c053ff4b9cf49a51fa81cff4d42
medina192 <ja.diazmedina@ugto.mx>
2021-06-07 02:15:14 -0500

    crash 3

--- index.js
+++ index.js
@@ -337,7 +337,7 @@ app.get('*', (req, res) => {
     res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
 });
 
-app.listen(process.env.PORT || 3002, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Port running on port ${process.env.PORT}`);
 });
 
